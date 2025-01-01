e31ba3d1b46ecd3049c3035e6be227ede2ab67e6
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 16:53:27 +0200

    Flutter <> images added

--- lib/Screens/Auth/SplashScreen.dart
+++ lib/Screens/Auth/SplashScreen.dart
@@ -13,37 +13,54 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           gradient: AppColor.linearGradient,
         ),
-        child: Column(
-          children: [
-            const Text("Welcome to SolarIQ", style: AppTextStyles.title),
-            const SizedBox(height: 16),
-            Buttons(
-              buttonText: "Get Started",
-              navigateTo: const Loginscreen(),
-              hasBorder: false,
-              backgroundColor: AppColor.buttonPrimary,
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Loginscreen()),
-                );
-              },
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildAnimatedImageRow(
+                  ['assets/images/1.png', 'assets/images/2.png'],
+                  [200, 400],
+                ),
+                const SizedBox(height: 16),
+                _buildAnimatedImageRow(
+                  ['assets/images/3.png', 'assets/images/4.png'],
+                  [600, 800],
+                ),
+                const SizedBox(height: 30),
+                const Text("Welcome to SolarIQ", style: AppTextStyles.title),
+                const SizedBox(height: 16),
+                Buttons(
+                  buttonText: "Get Started",
+                  navigateTo: const Loginscreen(),
+                  hasBorder: false,
+                  backgroundColor: AppColor.buttonPrimary,
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Loginscreen()),
+                    );
+                  },
+                ),
+                const SizedBox(height: 16),
+                const Buttons(
+                  buttonText: "About Us",
+                  navigateTo: SizedBox(),
+                  hasBorder: false,
+                  backgroundColor: AppColor.buttonSecondary,
+                  showDialogOnTap: true,
+                  dialogTitle: "SolarIQ",
+                  dialogContent:
+                      "SolarIQ Maximize your solar investment with SolarIQ. Our AI-powered app provides real-time monitoring, weather-based predictions, and expert advice. Connect with a community of solar enthusiasts and unlock the full potential of your system.Key Features:\nReal-time Monitorin \nAI-Powered Advice \nWeather Predictions \nCommunity & Support",
+                ),
+                const SizedBox(height: 30),
+              ],
             ),
-            const SizedBox(height: 16),
-            const Buttons(
-              buttonText: "About Us",
-              navigateTo: SizedBox(),
-              hasBorder: false,
-              backgroundColor: AppColor.buttonSecondary,
-              showDialogOnTap: true,
-              dialogTitle: "SolarIQ",
-              dialogContent: "SolarIQ Maximize your solar investment with SolarIQ. Our AI-powered app provides real-time monitoring, weather-based predictions, and expert advice. Connect with a community of solar enthusiasts and unlock the full potential of your system.Key Features:\nReal-time Monitorin \nAI-Powered Advice \nWeather Predictions \nCommunity & Support",
-            ),
-            const SizedBox(height: 30),
-          ],
+          ),
         ),
       ),
     );
@@ -55,7 +72,8 @@ Widget _buildAnimatedImageRow(List<String> imagePaths, List<int> delays) {
     mainAxisAlignment: MainAxisAlignment.center,
     children: List.generate(imagePaths.length, (index) {
       return Padding(
-        padding: EdgeInsets.only(right: index != imagePaths.length - 1 ? 16.0 : 0),
+        padding:
+            EdgeInsets.only(right: index != imagePaths.length - 1 ? 16.0 : 0),
         child: AnimatedImage(
           imagePath: imagePaths[index],
           delay: Duration(milliseconds: delays[index]),
