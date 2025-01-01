878d1ea129198466c9b6bbff37fa894ebeff5e8f
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 16:16:04 +0200

    Flutter <> animated image initState added

--- lib/Widgets/Common/Animatedimage.dart
+++ lib/Widgets/Common/Animatedimage.dart
@@ -11,10 +11,44 @@ class AnimatedImage extends StatefulWidget {
   _AnimatedImageState createState() => _AnimatedImageState();
 }
 
-class _AnimatedImageState extends State<AnimatedImage> {
+
+class _AnimatedImageState extends State<AnimatedImage>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _opacityAnimation;
   late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _opacityAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    ));
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0.0, 0.5),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    Future.delayed(widget.delay, () {
+      if (mounted) {
+        _controller.forward();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
