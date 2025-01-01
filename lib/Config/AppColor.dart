9660476256e63dad795518ddd960fe44223f8407
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 14:49:48 +0200

    Flutter <> AppColor added

---
+++ lib/Config/AppColor.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static const LinearGradient linearGradient = LinearGradient(
+    colors: [Colors.teal, Colors.indigo],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+}
---
+++ lib/Widgets/Common/Buttons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Buttons extends StatefulWidget {
+  const Buttons({super.key});
+
+  @override
+  State<Buttons> createState() => _ButtonsState();
+}
+
+class _ButtonsState extends State<Buttons> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
