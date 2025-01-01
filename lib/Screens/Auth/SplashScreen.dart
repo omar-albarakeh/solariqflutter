da202d54c0a6b2b4b2d2db3a872ac705ac07ed60
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 16:09:45 +0200

    Flutter <> splashscreen background added

--- lib/Screens/Auth/SplashScreen.dart
+++ lib/Screens/Auth/SplashScreen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../../Config/AppColor.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppColor.linearGradient,
+        ),
+      ),
+    );
   }
 }
