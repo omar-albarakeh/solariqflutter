116f3ef2893d2a16d9df68126a1b449890868693
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 14:54:02 +0200

    Flutter <> AppText added

---
+++ lib/Config/AppText.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:solariqflutter/Config/AppColor.dart';
+
+class AppTextStyles {
+  static const TextStyle appBarTitle = TextStyle(
+    fontFamily: 'Poppins',
+    color: AppColor.textWhite,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );}
