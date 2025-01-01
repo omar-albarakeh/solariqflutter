7305baa4c9c1449c637de4711eaf9640d88f2466
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 18:36:00 +0200

    Flutter <> httphelper processResponse added

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class HttpHelper {
-  
   static Map<String, dynamic> _processResponse(http.Response response) {
     if (response.statusCode == 200 || response.statusCode == 201) {
       return jsonDecode(response.body);
@@ -13,4 +12,18 @@ class HttpHelper {
       };
     }
   }
+  static Future<Map<String, dynamic>> postRequest(
+      Uri url, Map<String, dynamic> body, {Map<String, String>? headers}) async {
+    try {
+      final response = await http.post(
+        url,
+        headers: headers ?? {'Content-Type': 'application/json'},
+        body: jsonEncode(body),
+      );
+      return _processResponse(response);
+    } catch (e) {
+      return {'error': 'Network error: ${e.toString()}'};
+    }
+  }
+
 }
