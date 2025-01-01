57b0e8970b179f4bd9702d1595f7986102d76d38
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 17:26:42 +0200

    Flutter <> authServices Added

---
+++ lib/Services/AuthServices.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthServices extends StatefulWidget {
+  const AuthServices({super.key});
+
+  @override
+  State<AuthServices> createState() => _AuthServicesState();
+}
+
+class _AuthServicesState extends State<AuthServices> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
