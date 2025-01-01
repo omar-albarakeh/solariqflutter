cff6466f61147eaa89b53a9a1bdfb405474c1b11
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 17:41:55 +0200

    Flutter <> social button class added

---
+++ lib/Widgets/Common/SocialButtons.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SocialButton extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final String text;
+  final VoidCallback onTap;
+
+  const SocialButton({
+    required this.icon,
+    required this.color,
+    required this.text,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
