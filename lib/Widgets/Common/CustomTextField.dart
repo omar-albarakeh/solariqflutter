4f05aef673365c99700cef7fba0c2e15293fc2db
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 17:08:14 +0200

    Flutter <> customTextField added

---
+++ lib/Widgets/Common/CustomTextField.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final TextEditingController controller;
+
+  const CustomTextField({
+    Key? key,
+    required this.label,
+    required this.icon,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
