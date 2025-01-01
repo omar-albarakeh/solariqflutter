e4fab00d83d0b0e52bc1eaca07ec7419318d903d
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 14:36:26 +0200

    Flutter <> SolarIQ added

---
+++ lib/SolarIQ.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class SolarIQ extends StatelessWidget {
+  const SolarIQ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'SolarIQ',
+      home:,
+    );
+}
