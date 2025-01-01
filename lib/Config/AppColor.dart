411f44c0efdb215d201fde8e46be0989f67aff25
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 14:50:48 +0200

    Flutter <> AppColor updated

--- lib/Config/AppColor.dart
+++ lib/Config/AppColor.dart
@@ -6,4 +6,8 @@ class AppColor{
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
+
+  static const Color buttonPrimary = Colors.orange;
+  static const Color buttonSecondary = Colors.lightGreen;
+  static const Color buttonBorder = Color(0xFF686666);
 }
