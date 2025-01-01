b2e651fedc779f58a3775237925ae68227732724
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 15:02:12 +0200

    Flutter <> LightThemE added

---
+++ lib/Config/LightTheme.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:solariqflutter/Config/AppColor.dart';
+import 'package:solariqflutter/Config/AppText.dart';
+
+class AppTheme {
+  // Light Theme
+  static ThemeData get lightTheme {
+    return ThemeData(
+      brightness: Brightness.light,
+      primaryColor: AppColor.primary,
+      scaffoldBackgroundColor: AppColor.backgroundLight,
+      appBarTheme: const AppBarTheme(
+        color: AppColor.primary,
+        titleTextStyle: AppTextStyles.appBarTitle,
+        iconTheme: IconThemeData(color: AppColor.textWhite),
+      ),
+      textTheme: const TextTheme(
+        displayLarge: AppTextStyles.appBarTitle,
+        titleLarge: AppTextStyles.title,
+        labelLarge: AppTextStyles.subtitleText,
+        bodyLarge: AppTextStyles.bodyText,
+        titleMedium: AppTextStyles.subtitleText,
+        bodySmall: AppTextStyles.bodyText,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColor.buttonPrimary,
+          textStyle: AppTextStyles.bodyText,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: AppColor.primary),
+        ),
+        labelStyle: AppTextStyles.subtitleText.copyWith(
+          color: AppColor.textGray,
+        ),
+      ),
+    );
+  }
+}
