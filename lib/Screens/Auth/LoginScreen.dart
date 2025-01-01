834ce7430a2e5586c838763eaed9ecf9e5102f2d
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-01 17:40:47 +0200

    Flutter <> forget password added

--- lib/Screens/Auth/LoginScreen.dart
+++ lib/Screens/Auth/LoginScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../Config/AppColor.dart';
+import '../../Config/AppText.dart';
+import '../../Widgets/Common/CustomTextField.dart';
 
 class Loginscreen extends StatefulWidget {
   const Loginscreen({super.key});
@@ -10,6 +12,8 @@ class Loginscreen extends StatefulWidget {
 }
 
 class _LoginscreenState extends State<Loginscreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +30,26 @@ class _LoginscreenState extends State<Loginscreen> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   _buildLogo(),
+                  Text("Hello, Sign In!", style: AppTextStyles.title),
+                  const SizedBox(height: 24),
+                  _buildTextField(label: 'Email', icon: Icons.email, controller: _emailController),
+                  const SizedBox(height: 16),
+                  _buildTextField(label: 'Password', icon: Icons.lock, controller: _passwordController),
+                  const SizedBox(height: 8),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: TextButton(
+                    onPressed: () {
+                    },
+                    child: Text(
+                      "Forgot Password?",
+                      style: AppTextStyles.subtitleText.copyWith(color: AppColor.textWhite),
+                    ),
+                  ),
+                ),
+              ),
                 ]),
           ),
         ),
@@ -36,4 +60,7 @@ class _LoginscreenState extends State<Loginscreen> {
 
 Widget _buildLogo() {
   return Image.asset("assets/images/LOGO.png", width: 234, height: 224);
+}
+Widget _buildTextField({required String label, required IconData icon, required TextEditingController controller}) {
+  return CustomTextField(label: label, icon: icon, controller: controller);
 }
