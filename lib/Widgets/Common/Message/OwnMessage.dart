457bb798e791075591d7bcdf1344cbaf11197f97
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-04 00:36:17 +0200

    Flutter <> Message Card updated

---
+++ lib/Widgets/Common/Message/OwnMessage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class OwnMessageCard extends StatelessWidget {
+  
+  const OwnMessageCard({
+    super.key,
+    required this.message,
+    required this.time,
+  });
+
+  final String message;
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
