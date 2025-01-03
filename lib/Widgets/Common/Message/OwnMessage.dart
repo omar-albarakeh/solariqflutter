76807a3ebaf38b40739c87a9a1eadc358b9782ce
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-04 00:37:05 +0200

    Flutter <> ownMessage Card updated

--- lib/Widgets/Common/Message/OwnMessage.dart
+++ lib/Widgets/Common/Message/OwnMessage.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'MessageCard.dart';
+
 class OwnMessageCard extends StatelessWidget {
-  
   const OwnMessageCard({
     super.key,
     required this.message,
@@ -13,8 +14,19 @@ class OwnMessageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MessageCard(
+      message: message,
+      time: time,
+      alignment: Alignment.centerRight,
+      borderColor: const Color(0xFF6C63FF),
+      cardColor: const Color(0xFFE8F0FE),
+      textColor: const Color(0xFF003366),
+      timeColor: const Color(0xFF5A5A5A), 
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(19),
+        bottomLeft: Radius.circular(19),
+        bottomRight: Radius.circular(19),
+      ),
+    );
   }
-
 }
