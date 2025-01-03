157b7bb4fd1ff26a1fdce514b006fcd2ed8abd27
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-04 00:38:17 +0200

    Flutter <> replyMessage added

--- lib/Widgets/Common/Message/OwnMessage.dart
+++ lib/Widgets/Common/Message/OwnMessage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'MessageCard.dart';
 
 class OwnMessageCard extends StatelessWidget {
@@ -21,7 +20,7 @@ class OwnMessageCard extends StatelessWidget {
       borderColor: const Color(0xFF6C63FF),
       cardColor: const Color(0xFFE8F0FE),
       textColor: const Color(0xFF003366),
-      timeColor: const Color(0xFF5A5A5A), 
+      timeColor: const Color(0xFF5A5A5A),
       borderRadius: const BorderRadius.only(
         topLeft: Radius.circular(19),
         bottomLeft: Radius.circular(19),
---
+++ lib/Widgets/Common/Message/ReplyMessageCard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ReplyMessageCard extends StatelessWidget {
+  const ReplyMessageCard({
+    super.key,
+    required this.message,
+    required this.time,
+  });
+
+  final String message;
+  final String time;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
