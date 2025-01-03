8d4d2d1a9b3e551ca1e3d8bec81204c2efd9e754
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-04 00:38:54 +0200

    Flutter <> replyMessage updated

--- lib/Widgets/Common/Message/ReplyMessageCard.dart
+++ lib/Widgets/Common/Message/ReplyMessageCard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'MessageCard.dart';
+
 class ReplyMessageCard extends StatelessWidget {
   const ReplyMessageCard({
     super.key,
@@ -12,7 +14,19 @@ class ReplyMessageCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MessageCard(
+      message: message,
+      time: time,
+      alignment: Alignment.centerLeft,
+      borderColor: const Color(0xFF4CAF50),
+      cardColor: const Color(0xFFFFFDE7),
+      textColor: const Color(0xFF1B5E20),
+      timeColor: const Color(0xFF757575),
+      borderRadius: const BorderRadius.only(
+        topRight: Radius.circular(19),
+        bottomLeft: Radius.circular(19),
+        bottomRight: Radius.circular(19),
+      ),
+    );
   }
 }
