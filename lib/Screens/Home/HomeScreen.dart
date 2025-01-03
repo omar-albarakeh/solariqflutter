537794158c787bb6ecb89703c2225a31d4434b3f
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-03 21:48:34 +0200

    Flutter <> floating menue added to homescreen

--- lib/Screens/Home/HomeScreen.dart
+++ lib/Screens/Home/HomeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../Widgets/Home/CurvedNavBar.dart';
+import '../../Widgets/Home/FloatingMenue.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -21,7 +22,17 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ],
             ),
-
+            Positioned(
+              bottom: 80,
+              right: 5,
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxWidth: 70,
+                  maxHeight: 300,
+                ),
+                child: const FloatingMenu(),
+              ),
+            ),
           ]),
     );
   }
