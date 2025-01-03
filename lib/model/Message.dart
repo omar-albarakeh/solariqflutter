2148dfc5c7011e5e74f831149676654bd447c6ce
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-04 00:59:23 +0200

    Flutter <> message model updated

--- lib/model/Message.dart
+++ lib/model/Message.dart
@@ -4,5 +4,23 @@ class MessageModel{
   String time;
   String type;
 
-  MessageModel(this.message, this.time,this.type);
+  MessageModel({required this.message, required this.time, required this.type});
+
+
+  Map<String,dynamic> toJson(){
+    return {
+      "type": this.type,
+      "message": this.message,
+      "time": this.time,
+    };
+  }
+
+  factory  MessageModel.fromJson(Map<String,dynamic> json){
+    return  MessageModel(
+    type : json["type"],
+    message : json["message"],
+    time : json["time"],
+    );
+  }
+
 }
