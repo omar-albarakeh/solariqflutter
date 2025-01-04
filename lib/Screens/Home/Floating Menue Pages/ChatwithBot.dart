3b337f519f1f1c4c35fe2b008e11c34b1ad86a1b
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-04 11:31:17 +0200

    flutter <> chatpage updated

--- lib/Screens/Home/Floating Menue Pages/ChatwithBot.dart
+++ lib/Screens/Home/Floating Menue Pages/ChatwithBot.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:solariqflutter/Config/AppColor.dart';
 import 'package:solariqflutter/Config/AppText.dart';
+
+import '../../../Widgets/Common/CustomTextField.dart';
 
 class ChatWithBot extends StatefulWidget {
   const ChatWithBot({super.key});
+
 
   @override
   State<ChatWithBot> createState() => _ChatPageState();
@@ -21,6 +25,32 @@ class _ChatPageState extends State<ChatWithBot> {
           style:AppTextStyles.appBarTitle,
         ),
       ),
+      body:  _buildInputArea(),
     );
   }
+}
+Widget _buildInputArea() {
+  final TextEditingController _controller = TextEditingController();
+
+  return Align(
+    alignment: Alignment.bottomCenter,
+    child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 8),
+      child: Row(
+        children: [
+          Expanded(
+            child: CustomTextField(
+              label: 'Type a message...',
+              controller: _controller,
+              obscureText: false,
+              keyboardType: TextInputType.multiline,
+              width: double.infinity,
+              suffixIcon: null,
+            ),
+          ),
+          
+        ],
+      ),
+    ),
+  );
 }
