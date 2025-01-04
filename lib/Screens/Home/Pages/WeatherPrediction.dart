f249579daa2377a731bee86aa5d76abf424e1116
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-04 19:22:04 +0200

    weather page updated

--- lib/Screens/Home/Pages/WeatherPrediction.dart
+++ lib/Screens/Home/Pages/WeatherPrediction.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../Config/AppText.dart';
+import '../../../Widgets/Home/ThemeNotifier.dart';
 
 class Weatherprediction extends StatefulWidget {
   const Weatherprediction({super.key});
@@ -10,6 +14,27 @@ class Weatherprediction extends StatefulWidget {
 class _WeatherpredictionState extends State<Weatherprediction> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: theme.primaryColor,
+      title: const Text(
+        'Weather Prediction',
+        style: AppTextStyles.appBarTitle,
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            theme.brightness == Brightness.dark
+                ? Icons.dark_mode
+                : Icons.light_mode,
+          ),
+          onPressed: () {
+            Provider.of<ThemeNotifier>(context, listen: false).toggleTheme();
+          },
+        ),
+      ],
+    ));
   }
 }
