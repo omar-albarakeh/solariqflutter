aa69ac26acdc6c83547d649a76fbe3a05eccdaef
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-06 12:47:36 +0200

    Flutter <> Community page updated

--- lib/Screens/Home/Pages/CommunityPage.dart
+++ lib/Screens/Home/Pages/CommunityPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:solariqflutter/Screens/Home/Pages/ComunityPost.dart';
 import '../../../Config/AppText.dart';
 
 class CommunityPage extends StatefulWidget {
@@ -15,12 +15,32 @@ class _CommunityPageState extends State<CommunityPage> {
     final theme = Theme.of(context);
 
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: theme.primaryColor,
-      title: const Text(
-        'Community',
-        style: AppTextStyles.appBarTitle,
+      appBar: AppBar(
+        backgroundColor: theme.primaryColor,
+        title: const Text(
+          'Community',
+          style: AppTextStyles.appBarTitle,
+        ),
       ),
-    ));
+      body: Center(
+        child: Text(
+          'Test',
+          style: theme.textTheme.bodyLarge,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const CommunityPost(),
+            ),
+          );
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+    );
   }
 }
---
+++ lib/Screens/Home/Pages/ComunityPost.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CommunityPost extends StatefulWidget {
+  const CommunityPost({super.key});
+
+  @override
+  State<CommunityPost> createState() => _CommunityPostState();
+}
+
+class _CommunityPostState extends State<CommunityPost> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
