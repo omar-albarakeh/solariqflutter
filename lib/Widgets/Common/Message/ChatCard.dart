cfd6eea2af90759ebba788305509699cffd0bfca
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-06 21:58:32 +0200

    Flutter <> chatCard added

---
+++ lib/Widgets/Common/Message/ChatCard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Chatcard extends StatefulWidget {
+  const Chatcard({super.key});
+
+  @override
+  State<Chatcard> createState() => _ChatcardState();
+}
+
+class _ChatcardState extends State<Chatcard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
