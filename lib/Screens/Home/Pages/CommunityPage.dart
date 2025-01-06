70dc82f2838f0073df8b1a381bd3a27273a8e7a6
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-06 12:39:18 +0200

    Flutter <> Community page updated

--- lib/Screens/Home/Pages/CommunityPage.dart
+++ lib/Screens/Home/Pages/CommunityPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../Config/AppText.dart';
+
 class CommunityPage extends StatefulWidget {
   const CommunityPage({super.key});
 
@@ -10,6 +12,15 @@ class CommunityPage extends StatefulWidget {
 class _CommunityPageState extends State<CommunityPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: theme.primaryColor,
+      title: const Text(
+        'Community',
+        style: AppTextStyles.appBarTitle,
+      ),
+    ));
   }
 }
