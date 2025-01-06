6097228212fe1fd0fb884ba15236d56556dba64f
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-06 10:32:37 +0200

    Flutter  <> Market Page updated

--- lib/Screens/Home/Pages/MarketPage.dart
+++ lib/Screens/Home/Pages/MarketPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'CartPage.dart';
+
 class MarketPage extends StatefulWidget {
   const MarketPage({super.key});
 
@@ -10,6 +12,30 @@ class MarketPage extends StatefulWidget {
 class _MarketPageState extends State<MarketPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+            appBar: AppBar(
+          title: Text('Market'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CartPage()),
+                );
+              },
+            ),
+          ],
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'All'),
+              Tab(text: 'Inverters'),
+              Tab(text: 'Panels'),
+              Tab(text: 'Batteries'),
+            ],
+          ),
+        )));
   }
 }
