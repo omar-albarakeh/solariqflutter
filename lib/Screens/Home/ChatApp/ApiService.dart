6244a754e830c982baf482673dc1719ce4194565
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-06 23:38:12 +0200

    Flutter <> chatservice updated

--- lib/Screens/Home/ChatApp/ApiService.dart
+++ lib/Screens/Home/ChatApp/ApiService.dart
@@ -20,7 +20,26 @@ class ApiService {
         throw Exception('Token is invalid or expired. Please login again.');
       }
 
+      final response = await http.get(
+        Uri.parse('$baseUrl/auth/contacts'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
 
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        return data['data'];
+      } else if (response.statusCode == 401) {
+
+        print('Token used: $token');
+        throw Exception('Unauthorized: Please login again.');
+      } else {
+        throw Exception('Failed to load contacts. Status: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching contacts: $e');
+    }
   }
 }
 
