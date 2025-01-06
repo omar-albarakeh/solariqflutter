49d799bb44b900f67df258a3fc7b688b62e4eba5
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-06 10:53:43 +0200

    Flutter  <> Market page updated

--- lib/Screens/Home/Pages/CartPage.dart
+++ lib/Screens/Home/Pages/CartPage.dart
@@ -20,6 +20,53 @@ class _CartPageState extends State<CartPage> {
         'Cart',
         style: AppTextStyles.appBarTitle,
       ),
-    ));
+    ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            buildCartItem(),
+          ],
+        ),
+      ),);
   }
+}
+
+
+
+Widget buildCartItem() {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.grey[200],
+      borderRadius: BorderRadius.circular(16.0),
+    ),
+    padding: EdgeInsets.all(16.0),
+    child: Row(
+      children: [
+        Container(
+          width: 80,
+          height: 80,
+          color: Colors.grey,
+        ),
+        SizedBox(width: 16.0),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Trina panel',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+            ),
+            Text('\$81'),
+          ],
+        ),
+        Spacer(),
+        IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {
+          },
+        ),
+      ],
+    ),
+  );
 }
--- lib/Screens/Home/Pages/MarketPage.dart
+++ lib/Screens/Home/Pages/MarketPage.dart
@@ -16,35 +16,47 @@ class _MarketPageState extends State<MarketPage> {
     return DefaultTabController(
         length: 4,
         child: Scaffold(
-            appBar: AppBar(
-          title: Text('Market'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CartPage()),
-                );
-              },
+          appBar: AppBar(
+            title: Text('Market'),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Dialog(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0),
+                        ),
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          height: MediaQuery.of(context).size.height * 0.6,
+                          child: CartPage(),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+            bottom: TabBar(
+              tabs: [
+                Tab(text: 'All'),
+                Tab(text: 'Inverters'),
+                Tab(text: 'Panels'),
+                Tab(text: 'Batteries'),
+              ],
             ),
-          ],
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'All'),
-              Tab(text: 'Inverters'),
-              Tab(text: 'Panels'),
-              Tab(text: 'Batteries'),
+          ),
+          body: TabBarView(
+            children: [
+              MarketGrid(),
+              MarketGrid(),
+              MarketGrid(),
+              MarketGrid(),
             ],
           ),
-        ),
-            body: TabBarView(
-            children: [
-            MarketGrid(),
-              MarketGrid(),
-            MarketGrid(),
-            MarketGrid(),
-      ],
-    ),));
+        ));
   }
 }
