877dcf90a5b5eda18bb756782b07057877781b8f
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-09 00:40:56 +0200

    Flutter <> current Weather service added

--- lib/Services/OpenWeather.dart
+++ lib/Services/OpenWeather.dart
@@ -6,11 +6,24 @@ class WeatherService {
   final String _currentWeatherUrl;
   final String _fiveDayForecastUrl;
 
-  
+
   WeatherService(this.apiKey)
       : _currentWeatherUrl =
   'https://api.openweathermap.org/data/2.5/weather?q=saida&mode=json&appid=$apiKey',
         _fiveDayForecastUrl =
         'https://api.openweathermap.org/data/2.5/forecast?q=saida&mode=json&appid=$apiKey';
 
+  Future<Map<String, dynamic>> fetchCurrentWeather() async {
+    try {
+      final response = await http.get(Uri.parse(_currentWeatherUrl));
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        throw Exception('Failed to load current weather data');
+      }
+    } catch (e) {
+      print('Error fetching current weather: $e');
+      return {};
+    }
+  }
 }
