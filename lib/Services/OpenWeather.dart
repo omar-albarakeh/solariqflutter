939844ca1fb1669aeb05a0e1a65bd4992bf9ef3b
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-09 02:20:34 +0200

    Flutter <> open weather service updated

--- lib/Services/OpenWeather.dart
+++ lib/Services/OpenWeather.dart
@@ -1,30 +1,41 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:xml/xml.dart';
+import 'dart:convert';
+import 'package:xml/xml.dart' as xml;
 
-class WeatherService {
+class OpenWeatherService {
   final String apiKey;
   final String _currentWeatherUrl;
   final String _fiveDayForecastUrl;
 
-
-  WeatherService(this.apiKey)
+  OpenWeatherService(this.apiKey)
       : _currentWeatherUrl =
   'https://api.openweathermap.org/data/2.5/weather?q=saida&mode=json&appid=$apiKey',
         _fiveDayForecastUrl =
-        'https://api.openweathermap.org/data/2.5/forecast?q=saida&mode=json&appid=$apiKey';
+        'https://api.openweathermap.org/data/2.5/forecast?q=saida&mode=xml&appid=$apiKey';
+
 
   Future<Map<String, dynamic>> fetchCurrentWeather() async {
     try {
       final response = await http.get(Uri.parse(_currentWeatherUrl));
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
-        final temperature = data['main']['temp'];
-        final weatherCode = data['weather'][0]['id'];
 
-        return {'temperature': temperature, 'weatherCode': weatherCode};
+        // Extract required data
+        final temperature = data['main']['temp'];
+        final weatherMain = data['weather'][0]['main'];
+        final sunrise = data['sys']['sunrise'];
+        final sunset = data['sys']['sunset'];
+        final clouds = data['weather'][0]['description'];
+
+        return {
+          'temperature': temperature,
+          'weatherMain': weatherMain,
+          'sunrise': sunrise,
+          'sunset': sunset,
+          'clouds':clouds
+        };
       } else {
-        throw Exception('Failed to load current weather data');
+        throw Exception('Failed to load current weather data: ${response.statusCode}');
       }
     } catch (e) {
       print('Error fetching current weather: $e');
@@ -32,39 +43,42 @@ class WeatherService {
     }
   }
 
+
   Future<List<Map<String, dynamic>>> fetchFiveDayForecast() async {
     try {
       final response = await http.get(Uri.parse(_fiveDayForecastUrl));
       if (response.statusCode == 200) {
         final xmlData = response.body;
-
-        final List<Map<String, dynamic>> parsedData = _parseFiveDayForecast(xmlData);
-
+        final parsedData = _parseFiveDayForecast(xmlData);
         return parsedData;
       } else {
-        throw Exception('Failed to load 5-day forecast data');
+        throw Exception('Failed to load 5-day forecast data: ${response.statusCode}');
       }
     } catch (e) {
       print('Error fetching 5-day forecast: $e');
       return [];
     }
   }
-  List<Map<String, dynamic>> _parseFiveDayForecast(String xmlData) {
-    final document = XmlDocument.parse(xmlData);
 
+  List<Map<String, dynamic>> _parseFiveDayForecast(String xmlData) {
+    final document = xml.XmlDocument.parse(xmlData);
     final forecastData = <Map<String, dynamic>>[];
+
 
     final timeNodes = document.findAllElements('time');
     for (var timeNode in timeNodes) {
       final fromTime = timeNode.getAttribute('from');
-      final symbol = timeNode.findElements('symbol').first;
-      final weatherCode = symbol.getAttribute('number');
-      final temperature = timeNode.findElements('temperature').first.getAttribute('value');
+
+      final temperatureElement = timeNode.findElements('temperature').first;
+      final temperature = temperatureElement.getAttribute('value');
+
+      final cloudsElement = timeNode.findElements('clouds').first;
+      final cloudsValue = cloudsElement.getAttribute('value');
 
       forecastData.add({
         'time': fromTime,
-        'weatherCode': weatherCode,
         'temperature': temperature,
+        'cloudsValue': cloudsValue,
       });
     }
 
