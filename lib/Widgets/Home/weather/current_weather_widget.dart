3ea3970a0bf18c91257cd28029787bcc4455d6e1
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-13 00:21:06 +0200

    Flutter <> current_weather_widget added

---
+++ lib/Widgets/Home/weather/current_weather_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class current_weather_widget extends StatefulWidget {
+  const current_weather_widget({super.key});
+
+  @override
+  State<current_weather_widget> createState() => _current_weather_widgetState();
+}
+
+class _current_weather_widgetState extends State<current_weather_widget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
