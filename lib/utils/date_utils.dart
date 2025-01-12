a279ebc6da1d39be740fe25f5d840ef944542548
omar albarakeh <omaralbarakeh2@gmail.com>
2025-01-13 00:19:52 +0200

    Flutter <> date utils updated

--- lib/utils/date_utils.dart
+++ lib/utils/date_utils.dart
@@ -15,3 +15,8 @@ String formatDate(DateTime dateTime) {
   return "${dateTime.day.toString().padLeft(2, '0')}-${dateTime.month.toString().padLeft(2, '0')}-${dateTime.year}";
 }
 
+String formatTime(DateTime dateTime) {
+  final hour = dateTime.hour.toString().padLeft(2, '0');
+  final minute = dateTime.minute.toString().padLeft(2, '0');
+  return "$hour:$minute";
+}
